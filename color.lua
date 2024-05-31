2a6f66f060245fa3f366bb81ff75f93ead8e3510
WertMakesCode <166886580+WertMakesCode@users.noreply.github.com>
2024-05-30 19:42:05 -0500

    Update color.lua

--- color.lua
+++ color.lua
@@ -1,2 +1,6 @@
 script.parent.brickcolor = brickcolor.new("ReallyRed")
 // change the really red to whatever color you need
+--THE SCRIPT MUST BE THE CHILD OF THE BRICK FOR IT TO WORK
+
+
+--Created By WertMakes :D
